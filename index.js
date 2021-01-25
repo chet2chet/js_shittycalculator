db93dc0ada86080e7fa8f8253151d4eef882e015
chet2chet <75305759+chet2chet@users.noreply.github.com>
2021-01-25 14:16:32 +0100

    shitty calculator written in js. not much to say

---
+++ index.js
@@ -0,0 +1,31 @@
+function addition(){
+    var numOne = document.getElementById("num_1").value;
+    var numTwo = document.getElementById("num_2").value;
+
+    var result = +numOne + +numTwo;
+    alert("Result: " + result);
+} 
+
+function subtraction(){
+    var numOne = document.getElementById("num_1_2").value;
+    var numTwo = document.getElementById("num_2_2").value;
+
+    var result = +numOne - +numTwo;
+    alert("Result: " + result);
+} 
+
+function multiplication(){
+    var numOne = document.getElementById("num_1_3").value;
+    var numTwo = document.getElementById("num_2_3").value;
+
+    var result = +numOne * +numTwo;
+    alert("Result: " + result);
+} 
+
+function division(){
+    var numOne = document.getElementById("num_1_4").value;
+    var numTwo = document.getElementById("num_2_4").value;
+
+    var result = +numOne / +numTwo;
+    alert("Result: " + result);
+} 
